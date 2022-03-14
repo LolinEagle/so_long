16a5ca71e2783fdd6616a4bc62317579bc831e69
Francois RUSSO <frrusso@e1r13p5.42.fr>
2022-03-14 15:28:49 +0100

    save

---
+++ src/so_long.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frrusso <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 11:23:16 by frrusso           #+#    #+#             */
+/*   Updated: 2022/03/14 11:23:20 by frrusso          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "../libft/libft.h"
+# include <mlx.h>
+# include <math.h>
+# include <fcntl.h>// open
+# include <stdio.h>// perror
+# include <string.h>// strerror
+
+// so_long.c	1 functions
+
+// main.c		1 functions
+
+#endif
